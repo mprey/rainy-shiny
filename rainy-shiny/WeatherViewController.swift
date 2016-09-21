17b1d9a5a5e7d590a134a2abc3e0ce71b91360d7
Mason Prey <masonprey7@gmail.com>
2016-09-21 13:06:53 -0500

    layout WeatherViewController
    create UIView of today’s weather alone with UITableView on the
    following days

---
+++ rainy-shiny/WeatherViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherViewController.swift
+//  rainy-shiny
+//
+//  Created by Mason Prey on 9/20/16.
+//  Copyright © 2016 Mason Prey. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
