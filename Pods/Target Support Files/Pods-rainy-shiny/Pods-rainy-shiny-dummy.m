c7d18b62d71fcf474139dad9493b64a7b8de2fcf
Mason Prey <masonprey7@gmail.com>
2016-09-20 20:57:53 -0500

    install Alamofire pod

---
+++ Pods/Target Support Files/Pods-rainy-shiny/Pods-rainy-shiny-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_rainy_shiny : NSObject
+@end
+@implementation PodsDummy_Pods_rainy_shiny
+@end
---
+++ Pods/Target Support Files/Pods-rainy-shiny/Pods-rainy-shiny-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_rainy_shinyVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_rainy_shinyVersionString[];
+
